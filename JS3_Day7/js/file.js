aaeba33a2fdd15d5f4411f6f0f38bce0e79fa94b
Your NameRishi <rishirg312@gmail.com>
2024-11-09 16:38:29 +0530

    javascript

---
+++ JS3_Day7/js/file.js
@@ -0,0 +1,82 @@
+// let add = (a, b) => a + b;
+
+// let sum = add(5,50);
+
+// console.log(sum);
+
+// var a = {
+//     name: "rishi",
+//     codes: {
+//         python: "Good",
+//         ui_ux: "Good",
+//     },
+// };
+
+// console.log(a.codes.python);
+
+// let {
+//     name,
+//     codes: {
+//         python
+//     }
+// } = a;          // Object Desctucturing
+
+// console.log(name, "python:", python);
+
+// let arr = [1, 2, 3, 4, 5, 6];
+
+// console.log(arr[5]);
+
+// let [zeroth, , second] = arr; // Array Desctucturing
+
+// console.log(second);
+
+// for (let i in arr) {
+//     console.log(i, arr[i]);
+// }
+
+// let i = 0;
+
+// while (i <= 5) {
+    // console.log(i);
+//     i++;
+// }
+
+// do {
+//     console.log(i);
+//     i++;
+// } while(i <= 5);
+
+// let day = 1;
+
+// switch (day) {
+//     case 0 :
+//         console.log("Monday");
+//     case 1 :
+//         console.log("Tuesday");
+//     default:
+//         console.log("Undefined");
+// }
+
+// let a = [10, 20, 30];
+// let b =[];
+
+// for (let i in a) {
+//     console.log(i, a[i]);
+//     b[i] = a[i] * 2;
+// }
+// console.log(b);
+
+// let a = [10, 20, 30];
+
+// let c = a.map((value) => value * 2);
+
+// console.log(c);
+
+// let d = a.filter((value) => value > 20);
+
+// console.log(d);
+
+let a = [10 ,20, 30, 40, 50];
+
+console.log(a.indexOf(10) == 0);
